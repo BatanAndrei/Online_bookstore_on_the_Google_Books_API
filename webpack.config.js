332401adfd7cf7cebd8415f86c06cbe4fc27f598
BatanAndrei <yabatan777@gmail.com>
2023-06-02 22:37:23 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ let path = require('path');  //библиотека для пути
 module.exports = {
     entry: path.resolve(__dirname, "index.js"),    //исходный файл
     output: {
-        path: path.resolve(__dirname, 'output'), //output называем папку в которую собираем
-        filename: "all.js",                      // all.js называем файл в котором собираем
+        path: path.resolve(__dirname, 'bundle'), //output называем папку в которую собираем
+        filename: "bundle.js",                      // all.js называем файл в котором собираем
     },
     mode: "development",    // переключение между Dev и Prod и будет собранный файл минифицирован (dev=norm, prod=minific...)
 }
