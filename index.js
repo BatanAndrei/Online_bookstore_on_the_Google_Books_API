063b8dc2288d52ea62c9c7a2ffaff338cbb45a48
BatanAndrei <yabatan777@gmail.com>
2023-06-05 10:02:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import nav from "./js/navigation.js"; 
 import cont from "./js/content.js";
+import scc from "./dist/allStyles.css";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 let path = require('path');  //библиотека для пути
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // для плагина css, стили прописываются в отдельном файле, а не в тэге
 
 module.exports = {
     entry: path.resolve(__dirname, "index.js"),    //исходный файл
@@ -7,4 +8,10 @@ module.exports = {
         filename: "bundle.js",                      // all.js называем файл в котором собираем
     },
     mode: "development",    // переключение между Dev и Prod и будет собранный файл минифицирован (dev=norm, prod=minific...)
-}
+    module: {
+        rules: [            // правила для сборщикак css
+          { test: /\.css$/,       // regExp для пропуска определённых файлов
+           use: ['style-loader', 'css-loader'] }  // с начало преобразовывает css-loader, потом style-loader
+        ]
+      }
+};
