3e21f9a9e293d0a73b54e15111745d13b386ca3c
BatanAndrei <yabatan777@gmail.com>
2023-06-07 12:39:13 +0300

    initial commit

--- js/slider.js
+++ js/slider.js
@@ -20,41 +20,39 @@ export function initSlider(){
     initAutoplay();
 
 
-function initImage() {
+function initImage() {                                 // рисуем картинки слайдера
     images.forEach((image, index) => {
         let imageDiv = `<div class="image n${index} ${index === 0 ? "active" : ""}" style="background-image:url('${images[index].url}');" data-index="${index}"></div>`;
         sliderImages.innerHTML += imageDiv;
     });
 };
 
-function initDots() {
+function initDots() {                                  // рисуем точки переключения слайдера
     images.forEach((image, index) => {
         let dot = `<div class="dot n${index} ${index ===0 ? "active" : ""}" data-index="${index}"></div>`;
         sliderDots.innerHTML += dot;
     });
     sliderDots.querySelectorAll('.dot').forEach(dot => {
-        dot.addEventListener("click", function() {
-            moveSlider(this.dataset.index);
+        dot.addEventListener("click", function () {
+            moveSlider(this.dataset.index);            // передаём индекс в функцию поткаторому будет определяться клвсс active
         });
     });
 };
 
-function moveSlider(num) {
+function initAutoplay() {                                          //автоперелистывание слайдера
+    setInterval(() => {
+        let curNumber = +sliderImages.querySelector('.active').dataset.index;
+        let nextNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
+        moveSlider(nextNumber);
+    }, intervalSlider)
+}
+
+function moveSlider(num) {                                          //добавляем или убираем active
     sliderDots.querySelector('.active').classList.remove('active');
     sliderDots.querySelector('.n' + num).classList.add('active');
     sliderImages.querySelector('.active').classList.remove('active');
     sliderImages.querySelector('.n' + num).classList.add('active');
     };
-
-function initAutoplay() {
-    setInterval(() => {
-        let curNumber = +sliderImages.querySelector('.active').dataset.index;
-        let nextNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
-        moveSlider(nextNumber);
-        console.log(curNumber)
-    }, intervalSlider)
-    
-}
 };
 
 document.addEventListener("DOMContentLoaded", function() {
