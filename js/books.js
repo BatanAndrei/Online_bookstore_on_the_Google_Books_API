ccf647648fd47ae065a49ede29951278d3389150
BatanAndrei <yabatan777@gmail.com>
2023-06-12 18:33:09 +0300

    initial commit

--- js/books.js
+++ js/books.js
@@ -1,22 +1,27 @@
-export function initRequest() {
+
     const showCaseBooks = document.querySelector('.showecase-books');
 
-    const params = new URLSearchParams(window.location.search);
+    const param = {
+        'url': 'https://www.googleapis.com/books/v1/' ,
+        'q': 'subject:Architecture',
+        'key': 'AIzaSyA6rzxK7JdhGxWOanC61q6X0V7Ya71YS8E',
+        'printType': 'books',
+        'startIndex': 0,
+        'maxResults': 6,
+        'langRestrict': 'en'
+    }
+   /*  const params = new URLSearchParams(window.location.search);
     params.set('q', 'subject:Architecture');
     params.set('key', 'AIzaSyA6rzxK7JdhGxWOanC61q6X0V7Ya71YS8E');
     params.set('printType', 'books');
     params.set('startIndex', 0);
     params.set('maxResults', 6);
-    params.set('langRestrict', 'en');
+    params.set('langRestrict', 'en'); */
 
-    let resultUrl = params.toString(); 
-
-        const options = {
-            method: 'GET',
-            mode: 'cors'
-        };
-
-        fetch(`https://www.googleapis.com/books/v1/volumes?${resultUrl}`, options)
+    //let resultUrl = params.toString(); 
+    
+    export function initRequest() {
+        fetch(`${param.url}volumes?q=${param.q}&key=${param.key}&printType=${param.printType}&startIndex=${param.startIndex}&maxResults=${param.maxResults}&langRestrict=${param.langRestrict}`)
         .then((response) => {
        
             const result = response.json();  //запись нового promisa
