a57265d9efd0bc717655aabcaafeef8fd95f0de5
BatanAndrei <yabatan777@gmail.com>
2023-06-14 15:15:01 +0300

    initial commit

--- js/books.js
+++ js/books.js
@@ -9,6 +9,7 @@
     let nextLoadCat = '';
     let allBooks = [];
     let cartBooks = [];
+    let id = '';
 
     function addToCart(booksItem) {             // меняем состояние кнопки и перпеключаем класс
         let buttonsBuyNow = document.querySelectorAll('.btn_buy-now');
@@ -19,11 +20,27 @@
                
                     if(buttonBuy.classList.contains('btn_in_the_cart')) {
                         buttonBuy.innerText = 'In the cart';
+                        saveBooks(event);
                     }else{
                         buttonBuy.innerText = 'buy now';
-                    }
 
-                    saveBooks(event);
+                        for( let i = 0; i < allBooks.length; i++){
+                            let resultForremove = allBooks[i];
+                            resultForremove.forEach((item, index) => {
+                                id = item.id;
+                                if(id == event.target.dataset.btnbuy) {
+
+                                    cartBooks.forEach((item, index) => {
+                                        if(item.id == id) {
+                                            cartBooks.splice(index, 1);
+                                        }
+                                    })
+                                    console.log(cartBooks);
+                                    localStorage.setItem('inCart', JSON.stringify(cartBooks));
+                                }
+                            })
+                        }
+                    }
             })
         })
     }
@@ -32,17 +49,17 @@
         if(!ev.target.dataset.btnbuy) {
             return;
         }
-        //console.log(itm.id)
         //let bookInCart = JSON.parse(localStorage.getItem('inCart'));
         for( let i = 0; i < allBooks.length; i++){
-            let resultFor = allBooks[i];
-            resultFor.forEach(item => {
-                let id = item.id;
+            let resultForadd = allBooks[i];
+            resultForadd.forEach(item => {
+                id = item.id;
                 if(id == ev.target.dataset.btnbuy) {
-                    let found = resultFor.find(element => element.id === id);
+                    let found = resultForadd.find(element => element.id === id);
                     cartBooks.push(found)
                     localStorage.setItem('inCart', JSON.stringify(cartBooks)); 
-                    console.log(found)
+                    //console.log(found)
+                    //console.log(cartBooks);
                 }
             })
         } 
